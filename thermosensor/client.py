96cb7ad3298d1e7ee3c7fa51f3f47111be1f209a
haixuanTao <hai-xuan.tao@student.ecp.fr>
2021-05-22 18:44:21 +0200

    adding arm support

--- thermosensor/client.py
+++ thermosensor/client.py
@@ -6,20 +6,25 @@ from prometheus_client import start_http_server
 import time
 from reader import read_temperature, base_dir, device_folder, read_ph
 
+
 class CustomCollector(object):
     def collect(self):
         temperatures = GaugeMetricFamily(
-            "temperatures", "Temperature taken from sensors", labels=["temperature_sensor"]
+            "temperatures",
+            "Temperature taken from sensors",
+            labels=["temperature_sensor"],
         )
         for device in device_folder:
-            temperatures.add_metric([device.strip(base_dir)], read_temperature(device))
+            temperatures.add_metric(
+                [device.strip(base_dir)], read_temperature(device)
+            )
         yield temperatures
 
         phs = GaugeMetricFamily(
             "phs", "PH taken from sensors", labels=["ph_sensor"]
         )
         for i in range(8):
-            phs.add_metric([f'ph_channel_{i}'], read_ph(i))
+            phs.add_metric([f"ph_channel_{i}"], read_ph(i))
         yield phs
 
 
