d0b28a371af6c627f5b1ce297d1af1bf66a11458
Xavier Tao <tao.xavier@outlook.com>
2021-05-24 10:14:53 +0100

    better installation sh

--- installation.sh
+++ installation.sh
@@ -21,6 +21,12 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm install grafana-wakaze grafana/grafana
 
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+k delete svc grafana-wakaze
+k expose deploy grafana-wakaze --type LoadBalancer
+k apply -f https://raw.githubusercontent.com/haixuanTao/Thermosensor/master/grafana-ingress.yaml
 
 sudo modprobe w1-gpio
 sudo modprobe w1-therm
@@ -28,3 +34,12 @@ sudo sh -c "echo 'w1_gpio' >> /etc/modules"
 sudo sh -c "echo 'w1_therm' >> /etc/modules"
 cd /sys/bus/w1/devices/
 ls
+
+
+cd ~
+git clone https://github.com/haixuanTao/Thermosensor
+cd ~/Thermosensor
+pip3 install -r requirement.txt
+sudo cp thermosensor/client.service /etc/systemd/system/client.service
+sudo systemctl start client.service
+sudo systemctl enable client.service
